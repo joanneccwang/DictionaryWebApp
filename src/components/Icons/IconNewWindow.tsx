93aeb47db550a952f7eedbce72392a25c2439310
Joanne Wang <joanneccwang@gmail.com>
2024-06-13 15:37:14 +0800

    fix: fix icons

--- src/components/Icons/IconNewWindow.tsx
+++ src/components/Icons/IconNewWindow.tsx
@@ -10,9 +10,9 @@ const IconNewWindow = () => {
       <path
         fill='none'
         stroke='#838383'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
         d='M6.09 3.545H2.456A1.455 1.455 0 0 0 1 5v6.545A1.455 1.455 0 0 0 2.455 13H9a1.455 1.455 0 0 0 1.455-1.455V7.91m-5.091.727 7.272-7.272m0 0H9m3.636 0V5'
       />
     </svg>
--- src/components/Icons/IconSearch.tsx
+++ src/components/Icons/IconSearch.tsx
@@ -10,9 +10,9 @@ const IconSearch = () => {
       <path
         fill='none'
         stroke='#A445ED'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
         d='m12.663 12.663 3.887 3.887M1 7.664a6.665 6.665 0 1 0 13.33 0 6.665 6.665 0 0 0-13.33 0Z'
       />
     </svg>
